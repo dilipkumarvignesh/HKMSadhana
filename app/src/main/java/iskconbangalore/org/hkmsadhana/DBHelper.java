047107e2eef43e8697a12039ec3c791b0d2e491b
dilipkumarvignesh <dilipkumarvignesh@gmail.com>
2017-06-18 12:33:24 +0530

    first comit

---
+++ app/src/main/java/iskconbangalore/org/hkmsadhana/DBHelper.java
@@ -0,0 +1,105 @@
+package iskconbangalore.org.hkmsadhana;
+
+/**
+ * Created by i308830 on 6/16/17.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "SadhanaTracker.db";
+    public static final String CONTACTS_TABLE_NAME = "SadhanaUpdate";
+    public static final String CONTACTS_COLUMN_ID = "id";
+    public static final String CONTACTS_COLUMN_DATE = "date";
+//    public static final String CONTACTS_COLUMN_EMAIL = "email";
+//    public static final String CONTACTS_COLUMN_STREET = "street";
+//    public static final String CONTACTS_COLUMN_CITY = "place";
+//    public static final String CONTACTS_COLUMN_PHONE = "phone";
+//    private HashMap hp;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "create table SadhanaUpdate " +
+                        "(id integer primary key, date text, MA text, DA text, SB text)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS SadhanaUpdate");
+        onCreate(db);
+    }
+
+    public boolean insertSadhana (String date, String MA, String DA, String SA) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("date", date);
+        contentValues.put("MA", MA);
+        contentValues.put("DA", DA);
+        contentValues.put("SA", SA);
+
+        db.insert("SadhanaUpdate", null, contentValues);
+        return true;
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from Sadhana where id="+id+"", null );
+        return res;
+    }
+
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, CONTACTS_TABLE_NAME);
+        return numRows;
+    }
+
+    public boolean updateContact (Integer id, String date, String phone, String email, String street,String place) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("date", date);
+//        contentValues.put("phone", phone);
+//        contentValues.put("email", email);
+//        contentValues.put("street", street);
+//        contentValues.put("place", place);
+        db.update("SadhanaUpdate", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
+        return true;
+    }
+
+    public Integer deleteContact (Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("SadhanaUpdate",
+                "id = ? ",
+                new String[] { Integer.toString(id) });
+    }
+
+    public ArrayList<String> getSadhanaHistory() {
+        ArrayList<String> array_list = new ArrayList<String>();
+
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from SadhanaUpdate", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            array_list.add(res.getString(res.getColumnIndex(CONTACTS_COLUMN_DATE)));
+            res.moveToNext();
+        }
+        return array_list;
+    }
+}
---
+++ app/src/main/java/iskconbangalore/org/hkmsadhana/RemainderService.java
@@ -0,0 +1,60 @@
+package iskconbangalore.org.hkmsadhana;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class RemainderService extends Service {
+    public RemainderService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Query the database and show alarm if it applies
+
+        // I don't want this service to stay in memory, so I stop it
+        // immediately after doing what I wanted it to do.
+
+        Toast.makeText(getApplicationContext(), " Remainder Service Running.",
+                            Toast.LENGTH_LONG).show();
+
+     AlarmManager alarm = (AlarmManager)getSystemService(ALARM_SERVICE);
+        alarm.set(
+                // This alarm will wake up the device when System.currentTimeMillis()
+                // equals the second argument value
+                alarm.RTC_WAKEUP,
+                System.currentTimeMillis() + (1000 * 60 * 60), // One hour from now
+                // PendingIntent.getService creates an Intent that will start a service
+                // when it is called. The first argument is the Context that will be used
+                // when delivering this intent. Using this has worked for me. The second
+                // argument is a request code. You can use this code to cancel the
+                // pending intent if you need to. Third is the intent you want to
+                // trigger. In this case I want to create an intent that will start my
+                // service. Lastly you can optionally pass flags.
+                PendingIntent.getService(this, 0, new Intent(this, RemainderService.class), 0)
+        );
+        stopSelf();
+
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        // I want to restart this service again in one hour
+        AlarmManager alarm = (AlarmManager)getSystemService(ALARM_SERVICE);
+        alarm.set(
+                alarm.RTC_WAKEUP,
+                System.currentTimeMillis() + (1000 * 60 * 60),
+                PendingIntent.getService(this, 0, new Intent(this, RemainderService.class), 0)
+        );
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
