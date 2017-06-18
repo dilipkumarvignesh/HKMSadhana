250c7433cf3ffabb9f368c763e1c3d7f3ed08d3f
dilipkumarvignesh <dilipkumarvignesh@gmail.com>
2017-06-18 13:25:42 +0530

    test

--- app/src/main/java/iskconbangalore/org/hkmsadhana/MySadhanaHistoryRecyclerViewAdapter.java
+++ app/src/main/java/iskconbangalore/org/hkmsadhana/MySadhanaHistoryRecyclerViewAdapter.java
@@ -19,7 +19,7 @@ import iskconbangalore.org.hkmsadhana.dummy.DummyContent.DummyItem;
 public class MySadhanaHistoryRecyclerViewAdapter extends RecyclerView.Adapter<MySadhanaHistoryRecyclerViewAdapter.ViewHolder> {
 
     private final List<DummyItem> mValues;
-    private final OnListFragmentInteractionListener mListener;
+    private final OnListFragmentInteractionListener mListener     ;
 
     public MySadhanaHistoryRecyclerViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
         mValues = items;
